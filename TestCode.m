4cba1cc7556d40a541325d88637540917f9983b7
Shawn W <37631215+frontsnapk1ck@users.noreply.github.com>
2024-11-14 17:30:42 -0700

    update to ultrasonic

--- TestCode.m
+++ TestCode.m
@@ -13,7 +13,8 @@ motorrf = -54.8; %D
 motorlb = 20;
 motorrb = 18.5;
 
-threshold = 50;
+thresholdRightWall = 50;
+thresholdFrontWall = 25;
 
 brick.SetColorMode(2, 2);
 
@@ -23,16 +24,16 @@ while 1
     brick.MoveMotor('D', motorrf);
     
     %Get Sensor Readings
-    touch = brick.TouchPressed(3);
+    frontWallDist = brick.UltrasonicDist(3);
     color = brick.ColorCode(2);
-    distance = brick.UltrasonicDist(1);
+    rightWallDist = brick.UltrasonicDist(1);
     
     %Color Decisions
-    if color == 5                      %if color is red stop for 4 sec                   
+    if color == 5                      %if color is red stop for 4 sec
         disp('red');
         brick.StopMotor('AD', 'Brake'); %Brake to prevent going off course
         pause(4); %wait 4 seconds
-        brick.MoveMotor('A', motorlf); 
+        brick.MoveMotor('A', motorlf);
         brick.MoveMotor('D', motorrf);
         pause(0.5);
     elseif color == 2 || color == 3    %if color is blue or green, activate keyboard control
@@ -44,40 +45,40 @@ while 1
     end
     
     %Navigation
-    if distance > threshold                %if right wall falls away from right side
+    if rightWallDist > thresholdRightWall                %if right wall falls away from right side
         pause(0.6); %wait to get past wall
         brick.StopMotor('AD', 'Brake');
         brick.MoveMotor('A', -20);
         pause(2.3); %turning time
         brick.StopMotor('A', 'Brake');
-        brick.MoveMotor('A', motorlf); 
+        brick.MoveMotor('A', motorlf);
         brick.MoveMotor('D', motorrf);
         pause(2);
-    end 
-    if touch %if hit wall in front
+    end
+    if frontWallDist < thresholdFrontWall %if hit wall in front
         pause(1); %keep going forward for a short period of time in order to calibrate
         
         disp('touched');
         brick.StopMotor('AD');          %stop
-        dist = brick.UltrasonicDist(1); %get distance from right wall
+        % dist = brick.UltrasonicDist(1); %get distance from right wall
         brick.MoveMotor('A', motorlb);
         brick.MoveMotor('D', motorrb);
         pause(3.5); %time to back up from wall
         brick.StopMotor('AD', 'Brake'); %stop
         
         %theoretically should never get here if previous method right
-        if distance < threshold %if there is no wall on the right
-            brick.MoveMotor('D', -18.5); 
+        if rightWallDist < thresholdRightWall %if there is no wall on the right
+            brick.MoveMotor('D', -18.5);
             pause(2.5);
             brick.StopMotor('D', 'Brake');
-            brick.MoveMotor('A', motorlf); 
+            brick.MoveMotor('A', motorlf);
             brick.MoveMotor('D', motorrf);
             pause(2);
         else %if there is a wall on the right
             brick.MoveMotor('A', -21);
             pause(2.5);
             brick.StopMotor('A', 'Brake');
-            brick.MoveMotor('A', motorlf); 
+            brick.MoveMotor('A', motorlf);
             brick.MoveMotor('D', motorrf);
             pause(2);
         end
